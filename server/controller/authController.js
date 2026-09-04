820d93fc118b2c5c3fd1c968cb1fc609dbb01d35
Piyush0826 <piyushvkb0826@gmail.com>
2026-09-04 13:13:10 +0530

    changes

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -11,7 +11,9 @@ const getTransporter = () => {
     }
 
     return nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS.replace(/\s/g, '')
